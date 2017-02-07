929c796e7a38f7505f1e3f823215032ae41b0089
Julien Hadley Jack <gitkraken@jlhj.de>
2017-02-07 23:08:46 +0100

    added more museums

--- scraper/scraper/spiders/local.py
+++ scraper/scraper/spiders/local.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from pathlib import PurePath
+from pathlib import Path
 
 import scrapy
 import yaml
 
 from scraper import PROJECT_PATH
-from scraper.items import Museum, FirebaseItem
+from scraper.items import Museum
 
 
 class LocalSpider(scrapy.Spider):
     name = "local"
-    start_urls = [
-        (PurePath(PROJECT_PATH) / "data" / "data.yml").as_uri()
-    ]
+    start_urls = [file.as_uri() for file in (Path(PROJECT_PATH) / "data").glob("**/*.yml")]
+
+    custom_settings = {'ROBOTSTXT_OBEY': False}
 
     def parse(self, response):
         data = yaml.load(response.text)
-        for museum in data["museums"]:
+        for museum in data.get("museums", []):
             yield self.parse_museum(museum)
 
     def parse_museum(self, museum):
